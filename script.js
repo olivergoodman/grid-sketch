ee74c9f4d77b359e3dd468a4522397b0863a1db9
olivergoodman <ollygreatman@gmail.com>
2015-08-25 17:06:31 -0700

    basic grid
    I have a basic working sketch pad. A grid gets created from user input
    and the user can draw/make new grids.

--- script.js
+++ script.js
@@ -1,5 +1,35 @@
 //script for grid-sketch
+var width = $(window).width(); //use this to get window width
+var height = $(window).height(); //window height
+
+console.log('width is ' + width);
+console.log('height is ' + height);
 
 $(document).ready(function(){
-
+	$('body').append('<div class="container"></div>');
+	$('.container').prepend('<button onclick="makeGrid(); return false">New Grid</button>');
+	
 });
+
+//makes a square grid; size from user input
+function makeGrid(){
+	//remove and add the container/button again
+	$('.container').remove()
+  	$('body').append('<div class="container"></div>')
+  	$('.container').prepend('<button onclick="makeGrid(); return false">New Grid</button>');
+
+	var x = prompt("How wide/tall do you want your grid?");
+	for(var i = 0; i < x; i++){
+		$('.container').append('<ul></ul>');
+	}
+	for (var j = 0; j < x; j++){
+		$('ul').append('<li></li>');
+	}
+
+	//drawing
+	$('li').on('mouseenter', function(){
+		$(this).addClass('highlighted');
+	});
+}
+
+ 
